7e7dbe94d447f1ce01c00c81f286fbefa4da3cf4
Samanvaya <samanvaya.yagsen@gmail.com>
2021-05-02 19:46:16 +0530

    Express sessions part 2

--- app.js
+++ app.js
@@ -38,40 +38,23 @@ app.use(session({
   store:new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next){
 
   console.log(req.session);
-
   if(!req.session.user){
-
-    var authHeader=req.headers.authorization;
-
-    if(!authHeader){
-      var err = new Error('You are not authenticated');
-      res.setHeader('WWW-Authenticate','Basic');
-      err.status = 401;
-     return  next(err);
-    }
    
-    var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':'); // [0] has the basic written in it
-    var username = auth[0];
-    var password = auth[1];
-   
-    if(username='admin'&&password=='password'){
-      req.session.user='admin';
-      next(); // if matches pass to next middleware
-    }
-    else{
-      var err = new Error('You are not authenticated');
-      res.setHeader('WWW-Authenticate','Basic');
-      err.status = 401;
-      next(err);
-  
-    }
-
+    var err = new Error('You are not authenticated');
+    res.setHeader('WWW-Authenticate','Basic');
+    err.status = 401;
+  return  next(err);
+    
   }
+
   else {
-    if(req.session.user=='admin'){
+    if(req.session.user==='authenticated'){
       next();
     }
     else{
@@ -87,8 +70,6 @@ function auth(req, res, next){
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
